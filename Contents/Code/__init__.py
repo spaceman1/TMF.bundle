fa19f4a5ffbcc9d6e067e7500f6b90da2bb2fa95
ryan <ryan@Mac-Pro.local>
2011-04-12 20:53:13 -0400

    Might be fixed.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -27,144 +27,143 @@ PLUGIN_ICON_DEFAULT = "icon-default.png"
 ####################################################################################################
 
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, L("PLUGIN_TITLE"))
-  Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+	Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, L("PLUGIN_TITLE"))
+	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 
-  # Set the default MediaContainer attributes
-  MediaContainer.title1         = L("PLUGIN_TITLE")
-  MediaContainer.viewGroup      = "List"
-  MediaContainer.art            = R(PLUGIN_ARTWORK)
+	# Set the default MediaContainer attributes
+	MediaContainer.title1         = L("PLUGIN_TITLE")
+	MediaContainer.viewGroup      = "List"
+	MediaContainer.art            = R(PLUGIN_ARTWORK)
 
-  # Set the default cache time
-  HTTP.SetCacheTime(CACHE_INTERVAL)
+	# Set the default cache time
+	HTTP.SetCacheTime(CACHE_INTERVAL)
 
 ####################################################################################################
 
 def MainMenu():
-  dir = MediaContainer(noCache=True)
+	dir = MediaContainer(noCache=True)
 
-  if Prefs['showtvstreams']:
-    dir.Append(Function(DirectoryItem(TvStreams, title=L("TITLE_TV_STREAMS"), thumb=R(PLUGIN_ICON_DEFAULT))))
-  if Prefs['showvideoclips']:
-    dir.Append(Function(DirectoryItem(VideoClipsAtoZ, title=L("TITLE_VIDEO_CLIPS"), thumb=R(PLUGIN_ICON_DEFAULT))))
-  dir.Append(PrefsItem(L("TITLE_PREFERENCES"), thumb=R(PLUGIN_ICON_DEFAULT)))
+	if Prefs['showtvstreams']:
+		dir.Append(Function(DirectoryItem(TvStreams, title=L("TITLE_TV_STREAMS"), thumb=R(PLUGIN_ICON_DEFAULT))))
+	if Prefs['showvideoclips']:
+		dir.Append(Function(DirectoryItem(VideoClipsAtoZ, title=L("TITLE_VIDEO_CLIPS"), thumb=R(PLUGIN_ICON_DEFAULT))))
+	dir.Append(PrefsItem(L("TITLE_PREFERENCES"), thumb=R(PLUGIN_ICON_DEFAULT)))
 
-  return dir
+	return dir
 
 ####################################################################################################
 
 def TvStreams(sender):
-  dir = MediaContainer(title2=L("TITLE_TV_STREAMS"))
+	dir = MediaContainer(title2=L("TITLE_TV_STREAMS"))
 
-  feed = XML.ElementFromURL(TMF_TV_STREAMS_FEED, errors='ignore').xpath('//channel/item')
+	feed = XML.ElementFromURL(TMF_TV_STREAMS_FEED, errors='ignore').xpath('//channel/item')
 
-  for item in feed:
-    url   = item.xpath('./link')[0].text
-    title = item.xpath('./title')[0].text
-    thumb = item.xpath('./enclosure')[0].get('url')
+	for item in feed:
+		url   = item.xpath('./link')[0].text
+		title = item.xpath('./title')[0].text
+		thumb = item.xpath('./enclosure')[0].get('url')
 
-    dir.Append(WebVideoItem(url=url, title=title, thumb=thumb))
+		dir.Append(WebVideoItem(url=url, title=title, thumb=thumb))
 
-  return dir
+	return dir
 
 ####################################################################################################
 
 def VideoClipsAtoZ(sender):
-  dir = MediaContainer(title2=L("TITLE_VIDEO_CLIPS"))
+	dir = MediaContainer(title2=L("TITLE_VIDEO_CLIPS"))
 
-  # A to Z
-  chars = string.ascii_uppercase
-  for i in range(26):
-    dir.Append(Function(DirectoryItem(Artists, title=chars[i], thumb=R(PLUGIN_ICON_DEFAULT)), letter=chars[i]))
-    i += 1
+	# A to Z
+	chars = string.ascii_uppercase
+	for i in range(26):
+		dir.Append(Function(DirectoryItem(Artists, title=chars[i], thumb=R(PLUGIN_ICON_DEFAULT)), letter=chars[i]))
+		i += 1
 
-  # 0-9
-  dir.Append(Function(DirectoryItem(Artists, title='0-9', thumb=R(PLUGIN_ICON_DEFAULT)), letter='0-9'))
+	# 0-9
+	dir.Append(Function(DirectoryItem(Artists, title='0-9', thumb=R(PLUGIN_ICON_DEFAULT)), letter='0-9'))
 
-  return dir
+	return dir
 
 ####################################################################################################
 
 def Artists(sender, letter):
-  dir = MediaContainer(title1=L("TITLE_VIDEO_CLIPS"), title2=letter)
+	dir = MediaContainer(title1=L("TITLE_VIDEO_CLIPS"), title2=letter)
 
-  query = base64.b64encode(TMF_ARTISTS_QUERY % (letter.lower(), 1))
-  content = HTML.ElementFromURL(TMF_CONTENT % query, errors='ignore')
+	query = base64.b64encode(TMF_ARTISTS_QUERY % (letter.lower(), 1))
+	content = HTML.ElementFromURL(TMF_CONTENT % query, errors='ignore')
 
-  # Number of pages for this letter
-  pages = content.xpath('//div[@class="cb"]/a')
-  # Pagination is displayed twice on the page if there's more than 1 page, so we need to divide by 2.
-  # The -1 is needed because of the 'last page' link.
-  numPages = 1 if len(pages) == 0 else len(pages)/2-1
+	# Number of pages for this letter
+	pages = content.xpath('//div[@class="cb"]/a')
+	# Pagination is displayed twice on the page if there's more than 1 page, so we need to divide by 2.
+	# The -1 is needed because of the 'last page' link.
+	numPages = 1 if len(pages) == 0 else len(pages)/2-1
 
-  for i in range(1, numPages+1):
-    query = base64.b64encode(TMF_ARTISTS_QUERY % (letter.lower(), i))
-    content = HTML.ElementFromURL(TMF_CONTENT % query, errors='ignore').xpath('//div[@class="cb item"]')
+	for i in range(1, numPages+1):
+		query = base64.b64encode(TMF_ARTISTS_QUERY % (letter.lower(), i))
+		content = HTML.ElementFromURL(TMF_CONTENT % query, errors='ignore').xpath('//div[@class="cb item"]')
 
-    for item in content:
-      id = item.xpath('./div[@class="title"]/a')[0].get('href').split('/')[2]
-      artist = item.xpath('./div[@class="title"]/a')[0].text.strip()
+		for item in content:
+			id = item.xpath('./div[@class="title"]/a')[0].get('href').split('/')[2]
+			artist = item.xpath('./div[@class="title"]/a')[0].text.strip()
 
-      if Prefs['showhiresthumbs']:
-        thumb = HTML.ElementFromURL(TMF_ARTIST_PAGE % id, errors='ignore').xpath('//div[@class="groupPhotoMain"]')[0].get('style')
-        thumb = re.search(r'url\((.+)\);', thumb).group(1)
-      else:
-        # Find lo-res thumbs
-        thumb = item.xpath('.//img[@class="ib"]')[0].get('src')
+			if Prefs['showhiresthumbs']:
+				thumb = HTML.ElementFromURL(TMF_ARTIST_PAGE % id, errors='ignore').xpath('//div[@class="groupPhotoMain"]')[0].get('style')
+				thumb = re.search(r'url\((.+)\);', thumb).group(1)
+			else:
+				# Find lo-res thumbs
+				thumb = item.xpath('.//img[@class="ib"]')[0].get('src')
 
-      dir.Append(Function(DirectoryItem(Videos, title=artist, thumb=thumb), id=id, artist=artist))
+			dir.Append(Function(DirectoryItem(Videos, title=artist, thumb=thumb), id=id, artist=artist))
 
-  return dir
+	return dir
 
 ####################################################################################################
 
 def Videos(sender, id, artist):
-  dir = MediaContainer(title1=L("TITLE_VIDEO_CLIPS"), title2=artist)
+	dir = MediaContainer(title1=L("TITLE_VIDEO_CLIPS"), title2=artist)
+	content = HTTP.Request(TMF_ARTIST_VIDEOS % id)
+	if len(content) > 100:
+		content = XML.ElementFromString(content).xpath('//content/image')
 
-  content = HTTP.Request(TMF_ARTIST_VIDEOS % id)
-  if len(content) > 100:
-    content = XML.ElementFromString(content).xpath('//content/image')
+		for item in content:
+			videoId = item.xpath('./data')[0].text
+			videoId = re.search(r'Video\((.+)\);', videoId).group(1)
 
-    for item in content:
-      videoId = item.xpath('./data')[0].text
-      videoId = re.search(r'Video\((.+)\);', videoId).group(1)
+			title = item.xpath('./description')[0].text
+			if title.find(" - ") != -1:
+				title = title.split(" - ", 1)[1]
 
-      title = item.xpath('./description')[0].text
-      if title.find(" - ") != -1:
-        title = title.split(" - ", 1)[1]
+			screenshot = item.xpath('./path')[0].text.split("proxy.php?src=", 1)[1]
 
-      screenshot = item.xpath('./path')[0].text.split("proxy.php?src=", 1)[1]
+			dir.Append(Function(WebVideoItem(PlayVideo, title=title, thumb=screenshot), videoId=videoId))
 
-      dir.Append(Function(WebVideoItem(PlayVideo, title=title, thumb=screenshot), videoId=videoId))
-
-  return dir
+	return dir
 
 ####################################################################################################
 
 def PlayVideo(sender, videoId):
-  content = HTTP.Request(TMF_VIDEO_INFO % videoId)
-  if len(content) > 100:
-    # Changed xpath to a regex because the xml file sometimes isn't valid due to unencoded ampersands in text like "R&B" (and xpath fails)
-    # link = XML.ElementFromString(content).xpath('//media:content', namespaces={'media':'http://search.yahoo.com/mrss/'})[0].get('url')
-    link = re.search('<media:content.+?url="(.+?)">', content).group(1)
-    videoSrc = XML.ElementFromURL(link, errors='ignore').xpath('//rendition[src!=""]/src')[0].text
+	content = HTTP.Request(TMF_VIDEO_INFO % videoId).content
+	if len(content) > 100:
+		# Changed xpath to a regex because the xml file sometimes isn't valid due to unencoded ampersands in text like "R&B" (and xpath fails)
+		# link = XML.ElementFromString(content).xpath('//media:content', namespaces={'media':'http://search.yahoo.com/mrss/'})[0].get('url')
+		link = re.search(r'<media:content.+?url="(.+?)">', content).group(1)
+		videoSrc = XML.ElementFromURL(link, errors='ignore').xpath('//rendition[src!=""]/src')[0].text
+		Log(videoSrc)
+		if videoSrc.find("edgefcs") != -1:
+			(streamer,file) = videoSrc.split('/ondemand/')
+			streamer = streamer + "/ondemand"
+			file = file.split('?', 1)[0] # Lose the ?llnwd.net at the end of the string if it's there (why is it there in the first place?!?)
+			if file.find('.mp4') != -1:
+				file = 'mp4:' + file[:-4]
+			elif file.find('.flv') != -1:
+				file = file[:-4]
+			url = (PLEXAPP_RTMP_PLAYER_URL % (streamer,file))
+		elif videoSrc.find("llnwd") != -1:
+			partUrl = re.search('rtmp://(.+)\.flv', videoSrc).group(1)
+			partUrl = partUrl.rsplit('/', 1)
+			streamer = 'rtmp://' + partUrl[0]
+			file = partUrl[1]
+			url = (PLEXAPP_RTMP_PLAYER_URL % (streamer,file))
 
-    if videoSrc.find("edgefcs") != -1:
-      (streamer,file) = videoSrc.split('/ondemand/')
-      streamer = streamer + "/ondemand"
-      file = file.split('?', 1)[0] # Lose the ?llnwd.net at the end of the string if it's there (why is it there in the first place?!?)
-      if file.find('.mp4') != -1:
-        file = 'mp4:' + file[:-4]
-      elif file.find('.flv') != -1:
-        file = file[:-4]
-      url = (PLEXAPP_RTMP_PLAYER_URL % (streamer,file))
-    elif videoSrc.find("llnwd") != -1:
-      partUrl = re.search('rtmp://(.+)\.flv', videoSrc).group(1)
-      partUrl = partUrl.rsplit('/', 1)
-      streamer = 'rtmp://' + partUrl[0]
-      file = partUrl[1]
-      url = (PLEXAPP_RTMP_PLAYER_URL % (streamer,file))
-
-    return Redirect('plex://localhost/video/:/webkit?url=' + String.Quote(url, usePlus=True))
-  else:
-    return None
+		return Redirect('plex://localhost/video/:/webkit?url=' + String.Quote(url, usePlus=True))
+	else:
+		return None
